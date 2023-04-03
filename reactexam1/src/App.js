51abec9d71aa155f06a11a0d18d63136e20abf6e
jihojun1 <shj9288@naver.com>
2023-04-03 19:50:33 +0900

    한입 크기로 잘라 먹는 리액트 강의

---
+++ reactexam1/src/App.js
@@ -0,0 +1,14 @@
+// import "./App.css";
+import Counter from "./Counter";
+import MyHeader from "./MyHeader";
+
+function App() {
+  return (
+    <div className="App">
+      <MyHeader />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
